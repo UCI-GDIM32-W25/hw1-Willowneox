bb184c425063184c6a8cf21f364d77a06fde1c0c
Willowneox <sijiex3@uci.edu>
2025-01-11 13:52:35 -0800

    1/11

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,21 +8,52 @@ public class Player : MonoBehaviour
     [SerializeField] private int _numSeeds = 5; 
     [SerializeField] private PlantCountUI _plantCountUI;
 
-    private int _numSeedsLeft;
-    private int _numSeedsPlanted;
+    private int _numSeedsLeft = 5;
+    private int _numSeedsPlanted = 0;
 
     private void Start ()
     {
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        if (_playerTransform == null)
+        {
+            _playerTransform = this.transform;
+        }
         
     }
 
     private void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical, 0f) * _speed * Time.deltaTime;
+        _playerTransform.position += movement;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
+
     }
 
     public void PlantSeed ()
+    
     {
-        
+        if (_numSeedsLeft <= 0)
+        {
+            return;
+        }
+
+        if  (_numSeedsLeft>0)
+            Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+       
+        _numSeedsLeft--;
+        _numSeedsPlanted++;
+       
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+
+        Debug.Log($"Seeds Left: {_numSeedsLeft}, Seeds Planted: {_numSeedsPlanted}");
+       
     }
 }
